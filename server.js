1938b1de7ad33018aa9f0c6c36f0082f0efeaee5
Akash Singh <akash.singh01@nagarro.com>
2019-04-25 16:52:56 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const SERVER_PORT = process.env.PORT || 9090
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use('/',
+  express.static(__dirname + '/public')
+)
+app.listen(SERVER_PORT, function () {
+    console.log("Server started on http://localhost:3333");
+});
