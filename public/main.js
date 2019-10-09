f925552e34f2330fe12d65ff44fedf6000cf9045
akashsingh01 <akash.singh01@nagarro.com>
2019-10-09 16:20:46 +0530

    maked better

--- public/main.js
+++ public/main.js
@@ -608,7 +608,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\akashsingh01\Desktop\WeatherApp\WeatherApp\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! E:\Nagarro training archives\WeatherApp\WeatherApp\src\main.ts */"./src/main.ts");
 
 
 /***/ })
--- public/styles.js
+++ public/styles.js
@@ -598,8 +598,8 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\akashsingh01\Desktop\WeatherApp\WeatherApp\src\styles.css */"./src/styles.css");
-module.exports = __webpack_require__(/*! C:\Users\akashsingh01\Desktop\WeatherApp\WeatherApp\node_modules\bootstrap\dist\css\bootstrap.min.css */"./node_modules/bootstrap/dist/css/bootstrap.min.css");
+__webpack_require__(/*! E:\Nagarro training archives\WeatherApp\WeatherApp\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! E:\Nagarro training archives\WeatherApp\WeatherApp\node_modules\bootstrap\dist\css\bootstrap.min.css */"./node_modules/bootstrap/dist/css/bootstrap.min.css");
 
 
 /***/ })
