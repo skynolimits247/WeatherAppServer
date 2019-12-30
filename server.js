1e21778947ca60ffcdcbe9b172c6f1a0172e85c0
akash.singh01@nagarro.com <akash.singh01@nagarro.com>
2019-12-30 18:00:01 +0530

    update package.json

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const SERVER_PORT = process.env.PORT || 9090
+const SERVER_PORT = process.env.PORT || 9090;
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 app.use('/',
   express.static(__dirname + '/public')
-)
+);
 app.listen(SERVER_PORT, function () {
     console.log("Server started on http://localhost:3333");
 });
